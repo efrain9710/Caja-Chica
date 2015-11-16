226628ac7f2e08564b2a968b8f41a3d823ec7d26
Adrian <adrian_4862@hotmail.com>
2015-11-15 20:36:20 -0430

    Modificaciones a la base de datos

--- caja.sql
+++ caja.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2015 a las 08:00:53
--- Versión del servidor: 5.6.17
--- Versión de PHP: 5.5.12
+-- Servidor: localhost
+-- Tiempo de generación: 16-11-2015 a las 01:03:57
+-- Versión del servidor: 5.5.8
+-- Versión de PHP: 5.3.5
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -30,10 +29,10 @@ CREATE TABLE IF NOT EXISTS `cargo` (
   `id_cargo` int(11) NOT NULL AUTO_INCREMENT,
   `cargo` varchar(255) NOT NULL,
   PRIMARY KEY (`id_cargo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
--- Volcado de datos para la tabla `cargo`
+-- Volcar la base de datos para la tabla `cargo`
 --
 
 INSERT INTO `cargo` (`id_cargo`, `cargo`) VALUES
@@ -47,8 +46,9 @@ INSERT INTO `cargo` (`id_cargo`, `cargo`) VALUES
 (8, 'Consultor Lider 2'),
 (9, 'Consultor Lider 1'),
 (10, 'Soporte Tecnico 2'),
-(11, 'Servicios Generales'),
-(12, 'Analista RRHH');
+(13, 'Soporte Tecnico 1'),
+(14, 'Servicios Generales'),
+(15, 'Analista RRHH');
 
 -- --------------------------------------------------------
 
@@ -77,17 +77,17 @@ CREATE TABLE IF NOT EXISTS `factura` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=12 ;
 
 --
--- Volcado de datos para la tabla `factura`
+-- Volcar la base de datos para la tabla `factura`
 --
 
 INSERT INTO `factura` (`id_factura`, `n_factura`, `fecha_fac`, `fecha_carga`, `id_proveedor`, `id_personal`, `id_servicio`, `descri_fac`, `id_status`, `monto`) VALUES
-(5, 5, '2015-11-05', '2015-11-05', 1, 1, 1, 'asdas', 1, 23323),
-(6, 1, '2015-11-06', '2015-11-07', 2, 1, 1, 'sdasa', 1, 11212),
-(7, 6, '2015-11-06', '2015-11-08', 3, 2, 2, 'sdas', 3, 100),
-(8, 2, '2015-11-06', '2015-11-08', 2, 2, 2, 'sads', 3, 2000),
-(9, 8, '2015-11-08', '2015-11-08', 3, 1, 2, 'sda', 1, 300),
-(10, 6767, '2015-11-04', '2015-11-08', 3, 2, 2, 'dasdas', 3, 12),
-(11, 9, '2015-11-09', '2015-11-09', 3, 1, 1, 'asdsda', 3, 400);
+(1, 1, '2015-11-07', '2015-11-15', 3, 1, 1, 'sdasda', 4, 200),
+(2, 2, '2015-11-06', '2015-11-15', 1, 1, 4, 'sxsasxa', 1, 212),
+(3, 3, '2015-11-05', '2015-11-15', 1, 1, 4, 'sadsda', 3, 1212),
+(4, 4, '2015-11-12', '2015-11-15', 2, 1, 2, 'sasxas', 1, 21),
+(5, 5, '2015-11-06', '2015-11-15', 2, 1, 2, 'nnnn,mn', 3, 3434),
+(6, 6, '2015-11-15', '2015-11-15', 1, 1, 1, 'axz', 3, 21),
+(7, 8, '2015-11-14', '2015-11-15', 2, 3, 4, 'saas	', 3, 213);
 
 -- --------------------------------------------------------
 
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `monto` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=2 ;
 
 --
--- Volcado de datos para la tabla `monto`
+-- Volcar la base de datos para la tabla `monto`
 --
 
 INSERT INTO `monto` (`id_monto`, `monto`) VALUES
@@ -124,18 +124,18 @@ CREATE TABLE IF NOT EXISTS `personal` (
   `cargo` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_personal`),
   UNIQUE KEY `ci_per_2` (`ci_per`),
-  KEY `ci_per` (`ci_per`),
-  KEY `usuario` (`usuario`),
   KEY `usuario_2` (`usuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
 
 --
--- Volcado de datos para la tabla `personal`
+-- Volcar la base de datos para la tabla `personal`
 --
 
 INSERT INTO `personal` (`id_personal`, `usuario`, `ci_per`, `nom_per`, `ape_per`, `tele_per`, `cargo`) VALUES
-(1, 'gerente', '22440748', 'prueba', 'ape', '414231232', 'Gerente'),
-(2, 'victor', 'J-21212121-1', 'sdsads', 'sdasda', '4267228558', 'Coordinador de Infraestructura');
+(1, 'gerente', '22440748', 'jOSE', 'AGORREA', '4141234321', 'Gerente'),
+(2, 'victor', 'J-21212121-1', 'ROSA', 'YANES', '4147228558', 'Coordinador de Infraestructura'),
+(3, 'em', 'N-22345432', 'DRIAN', 'ASCANIO', '4142312323', 'Coordinador de Infraestructura'),
+(4, 'director', 'N-23131412', 'Yendry', 'Torres', '4142424234', 'Director');
 
 -- --------------------------------------------------------
 
@@ -150,16 +150,16 @@ CREATE TABLE IF NOT EXISTS `proveedor` (
   `tele_prove` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   `direcc_prove` text COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_proveedor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
 
 --
--- Volcado de datos para la tabla `proveedor`
+-- Volcar la base de datos para la tabla `proveedor`
 --
 
 INSERT INTO `proveedor` (`id_proveedor`, `rif_cedula`, `nom_prove`, `tele_prove`, `direcc_prove`) VALUES
-(1, '22447878', 'ds', '3212312', 'Proveedor del CNTI'),
-(2, '1231231', 'sadsas', '4141477477', 'Por ahi'),
-(3, 'J-12132132-1', 'Sotfwarsito', '4142312312', 'sdasdas');
+(1, '22447878', 'Panaderia El Carmen', '4142312123', 'Refrigerio, Tortas'),
+(2, '1231231', 'Taxis El Recreo', '4141477477', 'Linea para movilizarse a Chacaito, '),
+(3, 'J-12132132-1', 'Otros', '4142312312', 'no se encuantra dentro de la categoria');
 
 -- --------------------------------------------------------
 
@@ -171,19 +171,14 @@ CREATE TABLE IF NOT EXISTS `proyecto` (
   `id_proyecto` int(100) NOT NULL AUTO_INCREMENT,
   `nom_pro` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_proyecto`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=2 ;
 
 --
--- Volcado de datos para la tabla `proyecto`
+-- Volcar la base de datos para la tabla `proyecto`
 --
 
 INSERT INTO `proyecto` (`id_proyecto`, `nom_pro`) VALUES
-(1, 'pruebaaa'),
-(2, 'Prueba'),
-(3, 'p3'),
-(4, 'p6'),
-(5, 'peaa'),
-(6, '21');
+(1, 'Zurich');
 
 -- --------------------------------------------------------
 
@@ -198,17 +193,15 @@ CREATE TABLE IF NOT EXISTS `proye_geren` (
   PRIMARY KEY (`id_proge`),
   KEY `id_proyecto` (`id_proyecto`),
   KEY `id_gerente` (`id_gerente`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
 
 --
--- Volcado de datos para la tabla `proye_geren`
+-- Volcar la base de datos para la tabla `proye_geren`
 --
 
 INSERT INTO `proye_geren` (`id_proge`, `id_proyecto`, `id_gerente`) VALUES
-(1, 1, 1),
-(2, 4, 2),
-(3, 6, 2),
-(4, 1, 2);
+(1, 1, 3),
+(2, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -222,15 +215,19 @@ CREATE TABLE IF NOT EXISTS `servicio` (
   `descri_servi` text COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_servicio`),
   KEY `nom_servi` (`nom_servi`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=7 ;
 
 --
--- Volcado de datos para la tabla `servicio`
+-- Volcar la base de datos para la tabla `servicio`
 --
 
 INSERT INTO `servicio` (`id_servicio`, `nom_servi`, `descri_servi`) VALUES
-(1, 'Servicio de Correo', 'Servicio de Correo'),
-(2, 'Servicio de Prueba', 'Servicio de Prueba');
+(1, 'Servicios Publicos', 'Agua, luz, aseo, television satelital'),
+(2, 'Transporte', 'Taxis, transporte publico'),
+(3, 'Servicio Medicos', 'servicio medicos'),
+(4, 'Gastos Internos', 'gastos internos de la empresa'),
+(5, 'Viaticos', 'pagos previajes'),
+(6, 'Otros', 'Servicios que estan fuera de los predeterminados');
 
 -- --------------------------------------------------------
 
@@ -242,16 +239,17 @@ CREATE TABLE IF NOT EXISTS `status` (
   `id_status` int(11) NOT NULL AUTO_INCREMENT,
   `nom_sta` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_status`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
 
 --
--- Volcado de datos para la tabla `status`
+-- Volcar la base de datos para la tabla `status`
 --
 
 INSERT INTO `status` (`id_status`, `nom_sta`) VALUES
 (1, 'Aprobado'),
 (2, 'Rechazado'),
-(3, 'En proceso');
+(3, 'En proceso'),
+(4, 'Pagado');
 
 -- --------------------------------------------------------
 
@@ -266,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `tipo_usuario` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
 
 --
--- Volcado de datos para la tabla `tipo_usuario`
+-- Volcar la base de datos para la tabla `tipo_usuario`
 --
 
 INSERT INTO `tipo_usuario` (`id_tipo`, `nom_tipo`) VALUES
@@ -290,18 +288,20 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   KEY `tipo_usu` (`tipo_usu`),
   KEY `usuario` (`usuario`),
   KEY `usuario_2` (`usuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcar la base de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `tipo_usu`, `usuario`, `clave`, `correo_usu`) VALUES
 (1, 1, 'gerente', '11', 'gerente@gmail.com'),
-(2, 1, 'victor', '22', 'sda@sads.com');
+(2, 1, 'victor', '22', 'sda@sads.com'),
+(3, 2, 'em', '11', 'sads@sad.com'),
+(4, 2, 'director', '11', 'eqeqw@sa.com');
 
 --
--- Restricciones para tablas volcadas
+-- Filtros para las tablas descargadas (dump)
 --
 
 --
@@ -331,7 +331,3 @@ ALTER TABLE `proye_geren`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_4` FOREIGN KEY (`tipo_usu`) REFERENCES `tipo_usuario` (`id_tipo`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
